14da29f5e9f9df431b24ebc56b19d96c15f5da43
TarasSkorohodIO <167581010+TarasSkorohodIO@users.noreply.github.com>
2024-07-01 00:52:11 +0300

    р

--- demo/src/HapticFeedbackDemo.tsx
+++ demo/src/HapticFeedbackDemo.tsx
@@ -35,6 +35,7 @@ const HapticFeedbackDemo: FC = () => {
               >
                 <img
                     src={coinImage}
+                    className={"coin"}
                     alt="Coin"
                 />
               </div>
